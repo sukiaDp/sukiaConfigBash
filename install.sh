733b2acfc8a1bcbf81197b60412d23c58ece897a
sukiaDp <110688518+sukiaDp@users.noreply.github.com>
2025-03-07 20:40:33 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,26 +1,12 @@
 #!/bin/bash
-
-# 描述：自动切换PPA源并安装GCC 13、OpenCV和Boost库，然后切换至GCC 13
-
+# 描述：自动安装build-essential、OpenCV和Boost库
 set -e
 
 echo "更新软件包列表..."
 sudo apt-get update
 
-echo "检查并添加Ubuntu Toolchain测试PPA..."
-if ! grep -q "^deb .*/ubuntu-toolchain-r/test" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
-    sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-    sudo apt-get update
-fi
-
-echo "安装GCC 13和G++ 13..."
-sudo apt-get install gcc-13 g++-13 -y
-
-echo "配置GCC和G++为默认版本..."
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-13 60 --slave /usr/bin/g++ g++ /usr/bin/g++-13
-
-echo "切换默认GCC版本至GCC 13..."
-sudo update-alternatives --set gcc /usr/bin/gcc-13
+echo "安装build-essential..."
+sudo apt-get install build-essential -y
 
 echo "安装OpenCV库..."
 sudo apt-get install libopencv-dev -y
@@ -32,11 +18,11 @@ if ! grep -q "^deb .*/mhier/libboost-latest" /etc/apt/sources.list /etc/apt/sour
 fi
 
 echo "安装Boost库..."
-sudo apt install libboost1.81-dev -y
+sudo apt install libboost1.81-all-dev -y
 
 echo "验证安装版本..."
 gcc -v
 pkg-config --modversion opencv4
-dpkg -s libboost-all-dev | grep Version
+dpkg -s libboost1.81-all-dev | grep Version
 
-echo "GCC 13、OpenCV和Boost库安装并配置完毕。"
+echo "build-essential、OpenCV和Boost库安装并配置完毕。"
